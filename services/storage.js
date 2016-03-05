67bbfa73ad7b3ed9e4b7f70163ffd10785226690
TheConnMan <bcconn2112@gmail.com>
2016-03-05 09:30:15 -0500

    Implement storage put method

--- services/storage.js
+++ services/storage.js
@@ -1,6 +1,14 @@
+var fs = require('fs');
+
+var path = process.env.DATA_PATH || './relay';
+
 module.exports = {
 	put: function(client, payload) {
-		console.log("Putting payload for " + client);
+		var clientPath = path + '/' + client + '/';
+		if (!fs.existsSync(clientPath)) {
+			fs.mkdirSync(clientPath);
+		}
+		fs.writeFileSync(clientPath + new Date().getTime(), payload);
 	},
 	get: function(client) {
 		console.log("Getting payload for" + client);
